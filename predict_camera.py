92e62e666267bc3213c9fdc3fd425aa71f92af03
Egor <arangaduy@gmail.com>
2024-05-21 16:23:33 +0300

    added camera

---
+++ predict_camera.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import cv2
+
+CONFIDENCE_THRESHOLD = 0.8
+BLUE = (255, 0, 0)
+
+video_cap = cv2.VideoCapture(0)
+model = YOLO(r'runs\detect\yolov8n_custom\weights\best.pt')
+
+while True:
+    ret, frame = video_cap.read()
+    detections = model(frame)[0]
+    for data in detections.boxes.data.tolist():
+        confidence = data[4]
+
+        if float(confidence) < CONFIDENCE_THRESHOLD:
+            continue
+
+        xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
+        cv2.rectangle(frame, (xmin, ymin) , (xmax, ymax), BLUE, 2)
+        result = detections[0]
+        for box in result.boxes:
+            class_id = result.names[box.cls[0].item()]
+            cv2.putText(frame, class_id, (xmin, ymin + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+video_cap.release()
+cv2.destroyAllWindows()
