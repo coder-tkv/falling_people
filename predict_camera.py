ec130e8503c50128278f94391ba88e24d0270e0d
arangaduy-git <egahaker@mail.ru>
2024-05-21 21:13:07 +0300

    test 2 neural net - done

--- predict_camera.py
+++ predict_camera.py
@@ -1,14 +1,19 @@
 from ultralytics import YOLO
 import cv2
 
-CONFIDENCE_THRESHOLD = 0.8
+CONFIDENCE_THRESHOLD = 0.6
 BLUE = (255, 0, 0)
 
 video_cap = cv2.VideoCapture(0)
-model = YOLO(r'runs\detect\yolov8n_custom\weights\best.pt')
+model = YOLO(r'runs\detect\yolov8n_custom2\weights\best.pt')
 
 while True:
     ret, frame = video_cap.read()
+    width = 1500
+    height = 1080
+    dim = (width, height)
+
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
     detections = model(frame)[0]
     for data in detections.boxes.data.tolist():
         confidence = data[4]
@@ -17,11 +22,11 @@ while True:
             continue
 
         xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
-        cv2.rectangle(frame, (xmin, ymin) , (xmax, ymax), BLUE, 2)
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), BLUE, 2)
         result = detections[0]
         for box in result.boxes:
             class_id = result.names[box.cls[0].item()]
-            cv2.putText(frame, class_id, (xmin, ymin + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(frame, class_id + ' ' + str(round(confidence, 2)) + '%', (xmin, ymin + 23), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) == ord("q"):
