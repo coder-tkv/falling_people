8f9ce4f542b93c2d85d656e5d96dfbc03a907726
Arangaduy-git <egahaker@mail.ru>
2024-05-23 16:31:01 +0300

    server task done

---
+++ client_pc/main.py
@@ -0,0 +1,29 @@
+import socket
+from threading import Thread
+
+NAME = 'client_pc'
+HOSTNAME = 'localhost'
+PORT = 4578
+data = NAME
+
+client = socket.socket()
+print(f'[*] Connecting to {HOSTNAME}:{PORT} with name {NAME}')
+client.connect((HOSTNAME, PORT))
+print(f'[+] Connected with name {NAME}.')
+
+
+def listen_for_messages():
+    while True:
+        message = client.recv(1024).decode()
+        print(message)
+
+
+if __name__ == '__main__':
+    t = Thread(target=listen_for_messages)
+    t.daemon = True
+    t.start()
+    while data != 'stop':
+        client.sendall(data.encode('utf-8'))
+        data = input()
+    print('[-] Disconnected.')
+    client.close()
--- server/server.py
+++ server/server.py
@@ -12,42 +12,28 @@ print(f'[*] Listening as {SERVER_HOST}:{SERVER_PORT}')
 
 
 def listen_for_client(cs: socket.socket, ca: tuple, name: str):
-    if name == 'client_pc':
-        while True:
-            try:
-                message = cs.recv(1024).decode()
-                if not message:
-                    break
-                print(f'[!] {name}: {message}')
-            except WindowsError:
-                client_sockets.pop(cs)
+    while True:
+        try:
+            message = cs.recv(1024).decode()
+            if not message:
                 break
-            except Exception as e:
-                print(f'[!] Error: {e}')
-        print(f'[-] {ca} - {name}, disconnected.')
-    elif name == 'client_robot':
-        while True:
-            try:
-                message = cs.recv(1024).decode()
-                if not message:
-                    break
-                print(f'[!] {name}: {message}')
-            except WindowsError:
-                client_sockets.pop(cs)
-                break
-            except Exception as e:
-                print(f'[!] Error: {e}')
-        print(f'[-] {ca} - {name}, disconnected. ')
-    else:
-        print(f'[-] {ca} - {name}, disconnected. The client name is incorrect.')
-        cs.close()
+            for el in client_sockets:
+                el.sendall(f'[!] {name}: {message}'.encode('utf-8'))
+            print(f'[!] {name}: {message}')
+        except WindowsError:
+            client_sockets.pop(cs)
+            break
+        except Exception as e:
+            print(f'[E] {e}')
+    print(f'[-] {ca} - {name}, disconnected.')
 
 
-while True:
-    client_socket, client_address = server.accept()
-    received_name = str(client_socket.recv(1024).decode())
-    print(f'[+] {client_address} - {received_name}, connected.')
-    client_sockets[client_socket] = received_name
-    t = Thread(target=listen_for_client, args=(client_socket, client_address, received_name))
-    t.daemon = True
-    t.start()
+if __name__ == '__main__':
+    while True:
+        client_socket, client_address = server.accept()
+        received_name = str(client_socket.recv(1024).decode())
+        print(f'[+] {client_address} - {received_name}, connected.')
+        client_sockets[client_socket] = received_name
+        t = Thread(target=listen_for_client, args=(client_socket, client_address, received_name))
+        t.daemon = True
+        t.start()
